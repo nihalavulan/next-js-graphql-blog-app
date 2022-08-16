9c1eb8e19381f459cfad8c27d15e3e419b1d1b4d
Nihal-a <nihalavulan1@gamil.com>
2022-08-16 23:27:27 +0530

    Created components for post details

---
+++ pages/post/[slug].js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Author, Categories, Comments, CommentsForm, PostDetail, PostWidget } from '../../components'
+
+const PostDetails = () => {
+  return (
+    <div className='container mx-auto px-10 mb-8'>
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+            <div className="col-span-1 col-span-8">
+                <PostDetail />
+                <Author />
+                <CommentsForm />
+                <Comments />
+            </div>
+            <div className="col-span-1 col-span-4">
+                <div className="relative lg:sticky top-8">
+                <PostWidget />
+                <Categories />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default PostDetails
